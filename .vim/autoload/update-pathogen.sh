ce0b3158ad3c64fa76cdbea0b3ed4d09c07176cf
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2014-11-21 17:55:48 +0100

    vim addons

---
+++ .vim/autoload/update-pathogen.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
---
+++ .vimrc
@@ -0,0 +1,7 @@
+execute pathogen#infect()
+syntax on
+filetype plugin indent on
+
+:set  t_Co=256
+let g:rehash256 = 1
+colorscheme molokai
