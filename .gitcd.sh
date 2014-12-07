1072da916b22831d784ba4f84fc2f4263f886b41
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2014-12-07 23:12:35 +0100

    add `gcd` function to navigate to git repos quickly

---
+++ .gitcd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function gitcd () {
+  if [ -z "$1" ]; then
+    return 1
+  fi
+  NEWDIR=$(find ~ -path "*/$1/.git" -a -type d -exec echo "{}/.." \; | head -n 1)
+  if [ -z "${NEWDIR}" ]; then
+    return 1
+  fi
+  cd -P "${NEWDIR}"
+  echo "$(pwd -P)"
+}
