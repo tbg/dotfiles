4431a6234594b9a98a07352d2d91008d09f49d44
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2015-04-16 17:22:42 -0400

    add git completion

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 cd -P $(dirname $0)
 git submodule update --init --recursive
 git clean -f
-find .[a-z]* -maxdepth 0 -print -type d -a -path .git -prune -o -ok rm -Irf "${HOME}/{}" \; -exec cp -rf "{}" "${HOME}/{}" \;
+find .[a-z]* -maxdepth 0 -print -type d -a -path .git -prune -o -ok rm -rf "${HOME}/{}" \; -exec cp -rf "{}" "${HOME}/{}" \;
