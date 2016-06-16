5f23256b89e4c70894a8229672b102285e7fa14b
Tobias Schottdorf <tobias.schottdorf@gmail.com>
2016-06-16 09:58:51 -0400

    small tweaks

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 cd -P $(dirname $0)
 git submodule update --init --recursive
 git clean -f
-find .[a-z]* -maxdepth 0 -print -type d -a -path .git -prune -o -ok rm -rf "${HOME}/{}" \; -exec cp -rf "{}" "${HOME}/{}" \;
+
+find .[a-z]* -maxdepth 0 -print -type d -a -path .git -prune -o -ok diff "${HOME}/{}" "{}" \; -o -ok rm -rf "${HOME}/{}" \; -exec cp -rf "{}" "${HOME}/{}" \;
